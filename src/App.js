7fae0ac4492c1f0b9f5c5884a193f3830c6b5397
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-22 12:19:00 +0530

    Card linked, data fetehed and searching movie application is working fine

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com/?apikey=a7092097";
+
 const App = () => {
   const movie1 = {
     Poster: "https://m.media-amazon.com/images/M/MV5BYWM0MDI1ZmItZTYzNi00ZWVlLTg5MTctNzllNjY2ZTI3NDhhXkEyXkFqcGdeQXVyNDk5MjA2MQ@@._V1_SX300.jpg",
@@ -11,24 +12,38 @@ const App = () => {
     Year: "2018",
     imdbID: "tt4275958",
   };
+
+  const [movies, setMovies] = useState([]);
+  const [movieName, setMovieName] = useState("superman");
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=title`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
   useEffect(() => {
-    searchMovies("Spiderman");
+    searchMovies(movieName);
   }, []);
+  console.log(movies[0]);
+
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
-        <input type="text" placeholder="Search for movies" value="Superman" alt="search" />
-        <img src={SearchIcon} alt="search" />
+        <input type="text" placeholder="Search for movies" value={movieName} alt="search" onChange={(e) => setMovieName(e.target.value)} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(movieName)} />
       </div>
-      <div className="container">
-        <MovieCard movie={movie1} />
-      </div>
+      {/* {movies[0] && <MovieCard movie={movies[0]} />}; */}
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => {
+            return <MovieCard movie={movie} key={movie.imdbID} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
