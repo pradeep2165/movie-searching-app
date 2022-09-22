3abb9246575314cc672169107a8514066d5b6ce4
pradeep kumar swain <54628915+pradeep2165@users.noreply.github.com>
2022-09-22 11:00:32 +0530

    MovieCard component created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import "./App.css";
+import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 const API_URL = "http://www.omdbapi.com/?apikey=a7092097";
-
 const App = () => {
-  // {
-  //   Poster: "https://m.media-amazon.com/images/M/MV5BYWM0MDI1ZmItZTYzNi00ZWVlLTg5MTctNzllNjY2ZTI3NDhhXkEyXkFqcGdeQXVyNDk5MjA2MQ@@._V1_SX300.jpg";
-  //   Title: "Reign of Judges: Title of Liberty - Concept Short";
-  //   Type: "movie";
-  //   Year: "2018";
-  //   imdbID: "tt4275958";
-  // }
+  const movie1 = {
+    Poster: "https://m.media-amazon.com/images/M/MV5BYWM0MDI1ZmItZTYzNi00ZWVlLTg5MTctNzllNjY2ZTI3NDhhXkEyXkFqcGdeQXVyNDk5MjA2MQ@@._V1_SX300.jpg",
+    Title: "Reign of Judges: Title of Liberty - Concept Short",
+    Type: "movie",
+    Year: "2018",
+    imdbID: "tt4275958",
+  };
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=title`);
     const data = await response.json();
@@ -25,6 +25,9 @@ const App = () => {
       <div className="search">
         <input type="text" placeholder="Search for movies" value="Superman" alt="search" />
         <img src={SearchIcon} alt="search" />
+      </div>
+      <div className="container">
+        <MovieCard movie={movie1} />
       </div>
     </div>
   );
